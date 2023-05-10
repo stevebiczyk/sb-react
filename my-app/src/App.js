dd47bbd840f79cd3098f56eeac4424709c264bbe
stevebiczyk <stevebiczyk@gmail.com>
2023-05-10 22:21:36 +0000

    Added StatefulGreeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
-    <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" />
+    <div className='App'>
+      <StatefulGreeting greeting="I'm a stateful class component" />
     </div>
+    // <div className="App">
+    //   <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" />
+    // </div>
   );
 }
 
