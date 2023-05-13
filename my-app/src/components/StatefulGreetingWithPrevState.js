4acd237926e90bc791e1c4a989ba78ce7176562d
stevebiczyk <stevebiczyk@gmail.com>
2023-05-13 21:58:10 +0000

    Update the state, part 4

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -11,20 +11,24 @@ class StatefulGreetingWithCallback extends React.Component {
     }
 
     handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-        }, () => {
-            console.log('new state', this.state.introduction);
-            console.log('new state', this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState);
+            console.log("Previous Props:", prevProps);
+            return {
+                introduction:
+                    prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            };
         });
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
     }
 
     increment() {
-        this.setState({
-            count: this.state.count + 1
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState);
+            console.log("Previous Props:", prevProps);
+            return {
+                count: prevState.count + 1,
+            };
         });
     }
 
